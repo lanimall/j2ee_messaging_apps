4949746029b09a036646a87b00d19f7a4affb065
fabien.sanglier <fabiensanglier@gmail.com>
2018-02-02 10:01:06 -0500

    throw JMSException on the interface method

--- j2ee-jms-examples/SimpleJmsSend/ejb/src/main/java/com/softwareaggov/messaging/simplejmssendoneway/ejb/publish/JmsPublisherLocal.java
+++ j2ee-jms-examples/SimpleJmsSend/ejb/src/main/java/com/softwareaggov/messaging/simplejmssendoneway/ejb/publish/JmsPublisherLocal.java
@@ -1,10 +1,11 @@
 package com.softwareaggov.messaging.simplejmssendoneway.ejb.publish;
 
+import javax.jms.JMSException;
 import java.util.Map;
 
 /**
  * Created by fabien.sanglier on 6/15/16.
  */
 public interface JmsPublisherLocal {
-    String sendTextMessage(final String msgTextPayload, final Map<String, String> msgHeaderProperties);
+    String sendTextMessage(final String msgTextPayload, final Map<String, String> msgHeaderProperties) throws JMSException;
 }
--- j2ee-jms-examples/SimpleJmsSend/ejb/src/main/java/com/softwareaggov/messaging/simplejmssendoneway/ejb/publish/JmsPublisherRemote.java
+++ j2ee-jms-examples/SimpleJmsSend/ejb/src/main/java/com/softwareaggov/messaging/simplejmssendoneway/ejb/publish/JmsPublisherRemote.java
@@ -1,10 +1,13 @@
 package com.softwareaggov.messaging.simplejmssendoneway.ejb.publish;
 
+import javax.jms.JMSException;
 import java.util.Map;
 
 /**
  * Created by fabien.sanglier on 6/15/16.
  */
 public interface JmsPublisherRemote {
-    String sendTextMessage(final String msgTextPayload, final Map<String, String> msgHeaderProperties);
+    String sendTextMessage(final String msgTextPayload, final Map<String, String> msgHeaderProperties) throws JMSException;
+
+    ;
 }
