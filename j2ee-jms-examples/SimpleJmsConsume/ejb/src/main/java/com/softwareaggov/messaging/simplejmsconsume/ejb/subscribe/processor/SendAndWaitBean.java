912df8ab88afee6d394ac9322ce8ae0318c3fc99
fabien.sanglier <fabiensanglier@gmail.com>
2018-02-02 10:03:02 -0500

    perform a runtime jndi lookup for the needed beans (which are in another app) so that this app still deploy fine if these beans are not present in the app server (which may well happen, especially if these processors are not to be used)

--- j2ee-jms-examples/SimpleJmsConsume/ejb/src/main/java/com/softwareaggov/messaging/simplejmsconsume/ejb/subscribe/processor/SendAndWaitBean.java
+++ j2ee-jms-examples/SimpleJmsConsume/ejb/src/main/java/com/softwareaggov/messaging/simplejmsconsume/ejb/subscribe/processor/SendAndWaitBean.java
@@ -4,10 +4,17 @@ import com.softwareaggov.messaging.simplejmssendoneway.ejb.publish.JmsPublisherR
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.*;
+import javax.annotation.PostConstruct;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.util.*;
 
 /**
@@ -20,8 +27,32 @@ import java.util.*;
 public class SendAndWaitBean implements MessageProcessorLocal {
     private static Logger log = LoggerFactory.getLogger(SendAndWaitBean.class);
 
-    @EJB(beanName = "JmsSendAndWaitService", beanInterface = JmsPublisherRemote.class)
+//    @EJB(beanName = "JmsSendAndWaitService", beanInterface = JmsPublisherRemote.class)
+//    private JmsPublisherRemote jmsMessagePublisher;
+
+    // This will be injected dynamically by jndi lookup
     private JmsPublisherRemote jmsMessagePublisher;
+
+    final String appName = "SimpleJmsSend";
+    final String moduleName = "SimpleJmsSend-ejb";
+    final String beanName = "JmsSendAndWaitService";
+    final String viewClassName = JmsPublisherRemote.class.getName();
+
+    @PostConstruct
+    public void initialize() {
+        final Properties jndiProperties = new Properties();
+        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+
+        // create the context
+        final Context context;
+        try {
+            context = new InitialContext(jndiProperties);
+            jmsMessagePublisher = (JmsPublisherRemote) context.lookup("ejb:" + appName + "/" + moduleName + "/" + beanName + "!" + viewClassName);
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public Map.Entry<String, Map<String, String>> processMessage(Message msg) throws JMSException {
@@ -29,20 +60,24 @@ public class SendAndWaitBean implements MessageProcessorLocal {
 
             TextMessage txtMsg = (TextMessage) msg;
 
-            //copy the properties from the incoming message
-            HashMap props = new HashMap();
-            Enumeration txtMsgPropertiesEnum = txtMsg.getPropertyNames();
-            while (txtMsgPropertiesEnum.hasMoreElements()) {
-                String propName = (String) txtMsgPropertiesEnum.nextElement();
-                props.put(propName, txtMsg.getObjectProperty(propName));
+            if (null != jmsMessagePublisher) {
+                //copy the properties from the incoming message
+                HashMap props = new HashMap();
+                Enumeration txtMsgPropertiesEnum = txtMsg.getPropertyNames();
+                while (txtMsgPropertiesEnum.hasMoreElements()) {
+                    String propName = (String) txtMsgPropertiesEnum.nextElement();
+                    props.put(propName, txtMsg.getObjectProperty(propName));
+                }
+
+                //send the send and wait message
+                String textReturned = jmsMessagePublisher.sendTextMessage(txtMsg.getText(), Collections.unmodifiableMap(props));
+
+                return new AbstractMap.SimpleImmutableEntry<String, Map<String, String>>(
+                        textReturned, null
+                );
+            } else {
+                return null;
             }
-
-            //send the send and wait message
-            String textReturned = jmsMessagePublisher.sendTextMessage(txtMsg.getText(), Collections.unmodifiableMap(props));
-
-            return new AbstractMap.SimpleImmutableEntry<String, Map<String, String>>(
-                    textReturned, null
-            );
         } else {
             throw new RuntimeException("Received non-text message");
         }
