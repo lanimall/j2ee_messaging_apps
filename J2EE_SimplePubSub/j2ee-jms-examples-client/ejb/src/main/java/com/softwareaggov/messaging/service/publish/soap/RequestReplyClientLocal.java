070372e18a96653638039bfa5a71addd030359f8
fabien.sanglier <fabiensanglier@gmail.com>
2018-02-28 18:18:54 -0500

    removing old modules

--- J2EE_SimplePubSub/j2ee-jms-examples-client/ejb/src/main/java/com/softwareaggov/messaging/service/publish/soap/RequestReplyClientLocal.java
+++
@@ -1,9 +0,0 @@
-package com.softwareaggov.messaging.service.publish.soap;
-
-/**
- * Created by fabien.sanglier on 6/29/16.
- */
-public interface RequestReplyClientLocal {
-    Long getRandomNumber();
-    String performMultiplicationFromStrings(String nb1, String nb2);
-}
--- J2EE_SimplePubSub/j2ee-jms-examples-client/ejb/src/main/java/com/softwareaggov/messaging/service/soapclient/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://com.softwareaggov.messaging/j2ee-jms-examples/requestreply")
-package com.softwareaggov.messaging.service.soapclient;
