deb290db20c21b89568e8c23999cee0dc01b638a
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2018-06-29 23:59:47 -0400

    added copyright to all files

--- libs/src/main/java/com/softwareaggov/messaging/libs/utils/AppConfig.java
+++ libs/src/main/java/com/softwareaggov/messaging/libs/utils/AppConfig.java
@@ -1,15 +1,23 @@
 /*
- * Copyright 2012 by
  *
- * Software AG, Uhlandstrasse 12, D-64297 Darmstadt, GERMANY
  *
- * All rights reserved
+ *  Copyright © 2016 - 2018 Software AG, Darmstadt, Germany and/or its licensors
  *
- * This software is the confidential and proprietary
- * information of Software AG ('Confidential Information').
- * You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license
- * agreement you entered into with Software AG or its distributors.
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ * /
  */
 
 package com.softwareaggov.messaging.libs.utils;
