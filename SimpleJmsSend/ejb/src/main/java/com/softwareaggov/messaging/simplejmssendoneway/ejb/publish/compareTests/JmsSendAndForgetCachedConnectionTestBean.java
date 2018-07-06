91658fc3a6cacbc6eca9e0bf02d9e0390ee07488
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2018-07-06 11:29:57 -0400

    cleanup

--- SimpleJmsSend/ejb/src/main/java/com/softwareaggov/messaging/simplejmssendoneway/ejb/publish/compareTests/JmsSendAndForgetCachedConnectionTestBean.java
+++ SimpleJmsSend/ejb/src/main/java/com/softwareaggov/messaging/simplejmssendoneway/ejb/publish/compareTests/JmsSendAndForgetCachedConnectionTestBean.java
@@ -42,19 +42,8 @@ import javax.jms.Destination;
 public class JmsSendAndForgetCachedConnectionTestBean extends JmsSendAndForgetBean {
     private static Logger log = LoggerFactory.getLogger(JmsSendAndForgetCachedConnectionTestBean.class);
 
-    @Resource(name = "jms/someManagedCF")
-    private ConnectionFactory jmsConnectionFactory;
-
-    @Resource(name = "jms/someManagedDestination")
-    private Destination jmsDestination;
-
     @Override
     public ConnectionFactory getJmsConnectionFactory() {
         return new CachedConnectionFactory(jmsConnectionFactory);
-    }
-
-    @Override
-    public Destination getJmsDestination() {
-        return jmsDestination;
     }
 }
